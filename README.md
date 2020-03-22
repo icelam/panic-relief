30f1608094b917e0f58302daa88b6b828d5a7a81
Ice Lam <pinky.yy.lam@gmail.com>
2020-03-22 20:45:17 +0800

    feat: init commit - panic relief

---
+++ README.md
@@ -0,0 +1,25 @@
+# Panic Relief #
+
+![Key Visual](src/assets/images/og/og-image.png)
+
+### Installation ###
+
+* `yarn install`
+
+### Dev Server ###
+
+* `yarn dev`
+
+### Build Production ###
+
+All the build files can be found in `/dist` folder.
+
+* `yarn build`
+
+### Tech Stack ###
+
+* Pug
+* CSS3
+
+### Change Log ###
+* Click [here](CHANGELOG.md) to view change log
---
+++ src/index.js
@@ -0,0 +1,14 @@
+import 'core-js/stable';
+
+import '@style/index.scss';
+
+// PWA
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('./service-worker.js').then((registration) => {
+      console.log('SW registered: ', registration);
+    }).catch((registrationError) => {
+      console.log('SW registration failed: ', registrationError);
+    });
+  });
+}
